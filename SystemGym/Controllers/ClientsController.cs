5a0a62a993144fbb2d7e5f8999261f826a221211
Jhonatas Correa <5by5.jcorrea@voeazul.com.br>
2023-01-24 12:21:07 -0300

    Add project files.

---
+++ SystemGym/Controllers/ClientsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SystemGym.Controllers
+{
+    [Route("api/v1/Clients")]
+    public class ClientsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{Id}")]
+        public IActionResult GetById(string id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult RegisterClient([FromBody] string employee) //criar employee 
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateClient([FromBody] string employee)
+        {
+            //verificar se employee existe em banco de dados, se sim atualizar dados, se nao retornar erro
+            return Ok();
+
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteClientById([FromBody] string id) //criar deleteEmployee
+        {
+            //verificar se employee existe em banco de dados, se sim atualizar dados, se nao retornar erro
+            return Ok();
+        }
+    }
+}
---
+++ SystemGym/Controllers/EmployeesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SystemGym.Controllers
+{
+    [Route("api/v1/Employees")]
+    public class EmployeesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{Id}")]
+        public IActionResult GetById(string id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult RegisterEmployee([FromBody] string employee) //criar employee 
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateEmployee([FromBody] string employee)
+        {
+            //verificar se employee existe em banco de dados, se sim atualizar dados, se nao retornar erro
+            return Ok();
+
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteEmployeeById([FromBody] string id) //criar deleteEmployee
+        {
+            //verificar se employee existe em banco de dados, se sim atualizar dados, se nao retornar erro
+            return Ok();
+
+        }
+
+
+    }
+}
